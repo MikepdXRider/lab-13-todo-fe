26b57cb64b3b430f9fda25f80390d61e1f366735
Michael Rider <mikepdxrider@gmail.com>
2021-10-12 13:33:11 -0700

    CDD app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import {
+    BrowserRouter as Router, 
+    Route, 
+    Switch,
+} from 'react-router-dom';
+import SearchPage from './SearchPage.js';
+import DetailPage from './DetailPage.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css'
+
+// Magic string for local state here.
+
+export default class App extends Component {
+    // State with token set to retrieve local storage or emtpy string.
+
+    // handleTokenChange method which sets state and localstorage for token. Takes in token. 
+  
+  render() {
+    // destructure state
+    const { token } = this.state
+        return (
+            <div>
+                <Router>
+                    <Switch>
+                        <Route 
+                            path="/" 
+                            exact
+                            render={(routerProps) => <Homepage {...routerProps} />} 
+                        />
+                        {/* pass handleTokenChange to signup as prop */}
+                        <Route 
+                            path="/signup" 
+                            exact
+                            render={(routerProps) => <Signup
+                              // handleTokenChange = {handleTokenChange()}
+                              {...routerProps} />} 
+                        />
+                        {/* pass handleTokenChange to signin as prop */}
+                        <Route 
+                            path="/signin" 
+                            exact
+                            render={(routerProps) => <Signin
+                              // handleTokenChange = {handleTokenChange()}
+                              {...routerProps} />} 
+                        />
+                        {/* Pass token to todos page so it can perform fetch requests */}
+                        <Route 
+                          path="/todos/:Id" 
+                          exact
+                          render={(routerProps) => <TodosPage
+                            // token={token}
+                            {...routerProps} />} 
+                        />
+                    </Switch>
+                </Router>
+            </div>
+        )
+    }
 }
-
-export default App;
