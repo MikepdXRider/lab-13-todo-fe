839bd947e75c0d22467e958762863278ac734aa5
Michael Rider <mikepdxrider@gmail.com>
2021-10-12 14:46:18 -0700

    completes app.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {
     NavLink, 
     Route, 
     Switch,
+    Redirect
 } from 'react-router-dom';
 import Home from './components/Home.js'
 import Signin from './components/Signin.js'
@@ -12,23 +13,33 @@ import Todos from './components/Todos.js'
 
 import './App.css'
 
-// Magic string for local state here.
+// ✔ Magic string for local state here. 
+const TOKEN = 'TOKEN'
+
 export default class App extends Component {
-    // State with token set to retrieve local storage or emtpy string.
+    // ✔ State with token set to retrieve local storage or emtpy string.
+    state = {
+      token: localStorage.getItem(TOKEN) || ''
+    }
 
 
-    // handleTokenChange method which sets state and localstorage for token. Takes in token. 
+    // ✔ handleTokenChange method which sets state and localstorage for token. Takes in token.
+    handleTokenChange = async token => {
+      await this.setState(token);
+      await localStorage.setItem(TOKEN, token)
+    } 
   
 
   render() {
-    // destructure state
-    // const { token } = this.state
+    // ✔ destructure state
+    const { token } = this.state
         return (
             <div>
                 <Router>
+                    <NavLink exact to='/'>Home</NavLink>
                     <NavLink exact to='/signup'>Signup</NavLink>
                     <NavLink exact to='/signin'>Signin</NavLink>
-                    {/* Does this need the /:id as well?*/}
+                    {/* ❓ Does this need the /:id as well? ❓ */}
                     <NavLink exact to='/todos/'>todos</NavLink>
                     <Switch>
                         <Route 
@@ -36,31 +47,40 @@ export default class App extends Component {
                             exact
                             render={(routerProps) => <Home {...routerProps} />} 
                             />
-                        {/* pass handleTokenChange to signup as prop */}
+                        {/* ✔ pass handleTokenChange to signup as prop */}
                         <Route 
                             path="/signup" 
                             exact
-                            render={(routerProps) => <Signup
-                              // handleTokenChange = {handleTokenChange()}
-                              {...routerProps} />} 
-                              />
-                        {/* pass handleTokenChange to signin as prop */}
+                            render={(routerProps) =>
+                              <Signup
+                                handleTokenChange = { this.handleTokenChange }
+                                {...routerProps} 
+                              />} 
+                        />
+                        {/* ✔ pass handleTokenChange to signin as prop */}
                         <Route 
                             path="/signin" 
                             exact
-                            render={(routerProps) => <Signin
-                              // handleTokenChange = {handleTokenChange()}
-                              {...routerProps} />} 
-                              />
-                        {/* Pass token to todos page so it can perform fetch requests */}
-                        {/* Needs a conditional: If token doesn't exist, this redirects to signup page.*/}
+                            render={(routerProps) =>
+                              <Signin
+                                handleTokenChange = { this.handleTokenChange }
+                                {...routerProps} 
+                              />} 
+                        />
+                        {/* ✔ Pass token to todos page so it can perform fetch requests */}
+                        {/* ⚠ Needs a conditional: If token doesn't exist, this redirects to signup page.*/}
                         <Route 
                           path="/todos/:Id" 
                           exact
-                          render={(routerProps) => <Todos
-                            // token={token}
-                            {...routerProps} />} 
-                            />
+                          render={(routerProps) =>
+                            token
+                            ? <Todos
+                                token={token}
+                                {...routerProps} 
+                              />
+                            // ❗ This isn't working ❗
+                            : <Redirect to='/signup' />} 
+                        />
                     </Switch>
                 </Router>
             </div>
